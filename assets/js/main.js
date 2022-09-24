f95d3b5df0910a2f314089d48242f34994f7a996
Dnyanesh Kolhe <dnyaneshkolhe2@gmail.com>
2022-09-24 19:02:14 +0530

    comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
