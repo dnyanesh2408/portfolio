7f2f6a14db84c5e0300163b72dfb18af4f2d4043
Dnyanesh Kolhe <dnyaneshkolhe2@gmail.com>
2022-09-30 19:21:52 +0530

    comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,16 +105,6 @@ $('.navbar-nav>li>a').on('click', function(){
   $('.navbar-collapse').collapse('hide');
 });
 
-// on scroll nabvar hide
-// Store navbar classes
-// var navClasses = document.getElementById('navbar').classList;
 
-// function downAction() {
-//   navClasses.remove('open');
-//   navClasses.add('collapse');
-// }
+// =========================================================
 
-// function upAction() {
-//   navClasses.remove('collapse');
-//   navClasses.add('open');
-// }
